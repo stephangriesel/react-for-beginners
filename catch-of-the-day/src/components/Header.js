f1cb155d87d823bdcc105a823cd9bae7ef32c1e1
stephangriesel <sgriesel@gmail.com>
2019-03-14 06:48:47 +0100

    end vid 10

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,22 +1,40 @@
 import React from 'react';
 
-class Header extends React.Component {
-    render() {
-        return (
-            <header className="top">
-                <h1>Catch 
-                <span className="ofThe">
-                    <span className="of">Of</span> 
-                    <span className="The">The</span>
-                </span> 
-                Day</h1>
-                <h3 className="tagline">
-                <span>{this.props.tagline}</span>
-            </h3>
-            </header>
+// Functional component:
 
-        )
-    }
-}
+const Header = props => (
+    <header className="top">
+        <h1>Catch
+                <span className="ofThe">
+                <span className="of">Of</span>
+                <span className="The">The</span>
+            </span>
+            Day</h1>
+        <h3 className="tagline">
+            <span>{props.tagline}</span> {/* Functional Components does not use "this", get one argument props passed in via header */}
+        </h3>
+    </header>
+)
+
+// Class based component:
+
+// class Header extends React.Component {
+//     render() {
+//         return (
+//             <header className="top">
+//                 <h1>Catch 
+//                 <span className="ofThe">
+//                     <span className="of">Of</span> 
+//                     <span className="The">The</span>
+//                 </span> 
+//                 Day</h1>
+//                 <h3 className="tagline">
+//                 <span>{this.props.tagline}</span>
+//             </h3>
+//             </header>
+
+//         )
+//     }
+// }
 
 export default Header;
